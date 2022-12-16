ead0d10cf016bd2ffb85fa070d235eb1ace7ee53
CVrgas <usual389@gmail.com>
2022-12-16 17:30:16 -0400

    Angular Clock
    Angular, typescript, bootstrap, node.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,108 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { clock } from './clock.model';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
-  styles: []
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'clock';
+
+export class AppComponent implements OnInit {
+  //Clock
+  time: any;
+  Clock = true;
+  ngOnInit(): void {
+    let interval = setInterval(() =>{
+      this.time = new Date();
+    }, 1000);
+  }
+  
+  
+  //Stopwatch
+  StopWatch = false;
+  Sclock = 0;
+  Sminutes = 0;
+  Sseconds = 0;
+  reset = false;
+  resetWatch(){
+    this.reset = true
+    this.Sclock = 0;
+    this.Sminutes = 0;
+    this.Sseconds = 0;
+  }
+  startWatch(){
+
+    let interval = setInterval(() =>{
+      if (!this.reset) this.Sclock += 1;
+      if(this.reset) clearInterval(interval);
+      
+      this.Sminutes = Math.floor(this.Sclock/60);
+      this.Sseconds = this.Sclock - this.Sminutes*60;
+      
+    }, 1000)
+
+    this.reset = false
+  }
+  
+  
+  //timer
+  Timer = false;
+  InputValue = 0;
+  minutes = 0;
+  seconds = 0;
+  hours = 0;
+  stop = '';
+
+  LiveUpdate(){
+    let inputSeconds = this.InputValue * 60;
+    this.minutes = Math.floor(inputSeconds/60);
+    this.seconds = inputSeconds - this.minutes * 60;
+  }
+  setTimer(){
+    let inputSeconds = this.InputValue * 60;
+    this.stop = 'start'
+    if (inputSeconds > 0)
+    {
+      let interval = setInterval(()=>{
+        if (this.stop == 'stop') 
+        {
+          inputSeconds = 0;
+        }
+  
+        if(inputSeconds == 0 )
+        {
+          inputSeconds += 1;
+          clearInterval(interval);
+        }
+        inputSeconds -= 1;
+    
+        this.minutes = Math.floor(inputSeconds/60);
+        this.seconds = inputSeconds - this.minutes * 60;
+  
+      }, 1000) 
+    }
+  }
+  stopClock(){
+    this.stop = 'stop';
+  }
+
+
+//Hide and show
+  toClock(){
+    this.Clock = true;
+    this.StopWatch = false;
+    this.Timer = false
+
+  }
+  toStopWatch(){
+    this.Clock = false;
+    this.StopWatch = true;
+    this.Timer = false;
+
+  }
+  toTimer(){    
+    this.Clock = false;
+    this.StopWatch = false;
+    this.Timer = true;
+  }
 }
